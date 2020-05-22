6da17cc2f6882e149cfbb41a03a5d8ec5849c83e
shimanovAV <aleksandra.shimanovich@mail.ru>
2020-05-23 01:25:01 +0300

    upload photo first part

--- src/main/java/com/bsu/ourInstaProj/service/PhotoService.java
+++ src/main/java/com/bsu/ourInstaProj/service/PhotoService.java
@@ -2,9 +2,16 @@ package com.bsu.ourInstaProj.service;
 
 import com.bsu.ourInstaProj.dao.PhotoRepository;
 import com.bsu.ourInstaProj.entity.Photo;
+import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.StorageException;
+import com.microsoft.azure.storage.blob.*;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.*;
+import java.net.URISyntaxException;
+import java.security.InvalidKeyException;
 import java.util.List;
 
 @Service
@@ -22,12 +29,37 @@ public class PhotoService {
         return photoRepository.findAllByBoardId(boardId);
     }
 
-    public Photo addPhoto(Photo newPhoto) {
-        Photo photo = photoRepository.save(newPhoto);
-        if(photo!=null){
-            boardService.changeBoardPicture(photo.getBoardId(), photo.getUrl());
+    public String addPhoto(MultipartFile newPhoto, String boardId) {
+        String accountName = "bsulab7shimanovichav";
+        String accountKey = "3jL6HKnjuYs4p8sLbBtcyE6bYc92gRXfzcKzGRPeOuD5Zy7HYVHE7vprsHnv70EO6sQIB/MS7qhoJxQbFlaYIg==";
+        try {
+            String storageConnectionString = "DefaultEndpointsProtocol=http;" + "AccountName=" + accountName + ";" + "AccountKey=" + accountKey;
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+            CloudBlobContainer container = blobClient.getContainerReference("bsu");
+            InputStream is;
+            String contentType = newPhoto.getContentType();
+            if (contentType != null) {
+                is = new DataInputStream(newPhoto.getInputStream());
+                long length = newPhoto.getSize();
+                Photo lastPhoto = photoRepository.findFirstByOrderByIdDesc();
+                final String fileName;
+                if (lastPhoto != null) {
+                    Long uniquId = lastPhoto.getId() + 1;
+                    fileName = "image" + uniquId + ".jpg";
+                } else {
+                    fileName = "image1.jpg";
+                }
+                CloudBlockBlob blob = container.getBlockBlobReference(fileName);
+                blob.upload(is, length);
+                photoRepository.save(new Photo(null, Long.valueOf(boardId), null, blob.getUri().toString(), userService.findCurrentUser().getUsername()));
+                boardService.changeBoardPicture(Long.valueOf(boardId), blob.getUri().toString());
+                return blob.getUri().toString();
+            }
+        } catch (InvalidKeyException | URISyntaxException | StorageException | IOException ex) {
+            ex.printStackTrace();
         }
-        return photo;
+        return null;
     }
 
     @Transactional
